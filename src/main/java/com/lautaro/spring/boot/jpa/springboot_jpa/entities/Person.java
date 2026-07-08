be5a26f2f878f35e085f57c995e452800ecbf52b
Lautaro <lautarovanh@gmail.com>
2026-07-08 10:05:36 -0300

    Se cambio a update el ddl-auto

--- src/main/java/com/lautaro/spring/boot/jpa/springboot_jpa/entities/Person.java
+++ src/main/java/com/lautaro/spring/boot/jpa/springboot_jpa/entities/Person.java
@@ -7,61 +7,61 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "persons")
-public class Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String lastName;
+    @Entity
+    @Table(name = "persons")
+    public class Person {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private String lastname;
 
-    @Column(name = "programming_language")
-    private String programmingLanguage;
-
-
+        @Column(name = "programming_language")
+        private String programmingLanguage;
 
 
-    public Person() {
-    }
-    public Person(Long id, String name, String lastName, String programmingLanguage) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.programmingLanguage = programmingLanguage;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getProgrammingLanguage() {
-        return programmingLanguage;
-    }
-    public void setProgrammingLanguage(String programmingLanguage) {
-        this.programmingLanguage = programmingLanguage;
-    }
 
-    @Override
-    public String toString() {
-        return "[" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", programmingLanguage='" + programmingLanguage + '\'' +
-                ']';
-    }
+
+        public Person() {
+        }
+        public Person(Long id, String name, String lastname, String programmingLanguage) {
+            this.id = id;
+            this.name = name;
+            this.lastname = lastname    ;
+            this.programmingLanguage = programmingLanguage;
+        }
+        public Long getId() {
+            return id;
+        }
+        public void setId(Long id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getLastname() {
+            return lastname;
+        }
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+        public String getProgrammingLanguage() {
+            return programmingLanguage;
+        }
+        public void setProgrammingLanguage(String programmingLanguage) {
+            this.programmingLanguage = programmingLanguage;
+        }
+
+        @Override
+        public String toString() {
+            return "[" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", programmingLanguage='" + programmingLanguage + '\'' +
+                    ']';
+        }
 }
